2917eaaf1810cc7bdee6cb0b01def7f47f6b61c0
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-05-31 14:18:37 +0530

    In This i am making the Navbar and Authentication process via Login or Logout buttons.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import ContactPage from './pages/ContactPage/ContactPage';
+import AboutPage from './pages/AboutPage/AboutPage';
+import Menu from './pages/Menu/Menu';
+import Cart from './pages/Cart/Cart';
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline text-indigo-600">
-        Hello world!
-      </h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/contact' element={<ContactPage />} />
+      <Route path='/about' element={<AboutPage />} />
+      <Route path='/menu' element={<Menu />} />
+      <Route path='/cart' element={<Cart />} />
+
+
+
+      <Route path='/login' element={<Home />} />
+    </Routes>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { CartProvider } from './CartContext/CartContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <>
-    <App />
-  </>,
+  <CartProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </CartProvider>
 )
